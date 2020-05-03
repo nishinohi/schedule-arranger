553e0a7780e4080452c07ba3ccd1c484b905b987
nishinohi <tomoya0209@gmail.com>
2020-05-04 01:21:49 +0900

    スケジュール作成、確認機能実装

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ var helmet = require('helmet');
 var session = require('express-session');
 var passport = require('passport');
 
+// モデルの読み込み
+var User = require('./models/user');
+var Schedule = require('./models/schedule');
+var Availability = require('./models/availability');
+var Candidate = require('./models/candidate');
+var Comment = require('./models/comment');
+User.sync().then(() => {
+  Schedule.belongsTo(User, { foreignKey: 'createdBy' });
+  Schedule.sync();
+  Comment.belongsTo(User, { foreignKey: 'userId' });
+  Comment.sync();
+  Availability.belongsTo(User, { foreignKey: 'userId' });
+  Candidate.sync().then(() => {
+    Availability.belongsTo(Candidate, { foreignKey: 'candidateId' });
+    Availability.sync();
+  });
+});
+
 var GitHubStrategy = require('passport-github2').Strategy;
 var GITHUB_CLIENT_ID = 'eba3bb1bb38a92e14e6e';
 var GITHUB_CLIENT_SECRET = '8d2ce95350dd472083a9380caa5846f08b518324';
@@ -23,12 +41,20 @@ passport.use(new GitHubStrategy({
   clientSecret: GITHUB_CLIENT_SECRET,
   callbackURL: 'http://localhost:8000/auth/github/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  process.nextTick(() => done(null, profile));
+  process.nextTick(() => {
+    User.upsert({
+      userId: profile.id,
+      username: profile.username
+    }).then(() => {
+      done(null, profile);
+    });
+  });
 }));
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var logoutRouter = require('./routes/logout');
+var schedulesRouter = require('./routes/schedules');
 
 var app = express();
 app.use(helmet());
@@ -50,6 +76,7 @@ app.use(passport.session());
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
+app.use('/schedules', schedulesRouter);
 
 app.get('/auth/github',
   passport.authenticate('github', { scope: ['user:email'] }),
--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+let User = require('../models/user');
+let Schedule = require('../models/schedule');
+let Candidate = require('../models/candidate');
 
 describe('/login', () => {
     before(() => {
@@ -12,7 +15,7 @@ describe('/login', () => {
     after(() => {
         passportStub.logout();
         passportStub.uninstall(app);
-    })
+    });
 
     it('ログインのためのリンクが含まれる', (done) => {
         request(app)
@@ -35,4 +38,59 @@ describe('/login', () => {
             .expect('Location', '/')
             .expect(302, done);
     })
+});
+
+describe('/schedules', () => {
+    before(() => {
+        passportStub.install(app);
+        passportStub.login({ id: 0, username: 'testuser' });
+    });
+
+    after(() => {
+        passportStub.logout();
+        passportStub.uninstall(app);
+    });
+
+    it('予定が作成でき、表示される', (done) => {
+        User.upsert({ userId: 0, username: 'testuser' }).then(() => {
+            request(app)
+                .post('/schedules')
+                .send({ scheduleName: 'テスト予定1', memo: 'テストメモ1\r\nテストメモ2', candidates: 'テスト候補1\r\nテスト候補2\r\nテスト候補3' })
+                .expect('Location', /schedules/)
+                .expect(302)
+                .end((err, res) => {
+                    const createdSchedulePath = res.headers.location;
+                    request(app)
+                        .get(createdSchedulePath)
+                        .expect(/テスト予定1/)
+                        .expect(/テストメモ1/)
+                        .expect(/テストメモ2/)
+                        .expect(/テスト候補1/)
+                        .expect(/テスト候補2/)
+                        .expect(/テスト候補3/)
+                        .expect(200)
+                        .end((err, res) => {
+                            if (err) return done(err);
+                            // テストで作成したデータを削除
+                            const scheduleId = createdSchedulePath.split('/schedules/')[1];
+                            Candidate.findAll({
+                                where: { scheduleId: scheduleId }
+                            }).then((candidates) => {
+                                const promises = candidates.map((c) => { return c.destroy(); });
+                                Promise.all(promises).then(() => {
+                                    Schedule.findById(scheduleId).then((s) => {
+                                        s.destroy().then(() => {
+                                            if (err) return done(err);
+                                            done();
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    if (err) return done(err);
+                    done();
+                });
+        });
+    });
+
 });
