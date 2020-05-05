23ae44560e059c49b1e22917c1a9c13ceea3dd4b
nishinohi <tomoya0209@gmail.com>
2020-05-05 17:08:19 +0900

    出欠更新機能実装

---
+++ app/entry.js
@@ -0,0 +1,20 @@
+'use strict';
+import $ from 'jquery';
+
+$('.availability-toggle-button').each((i, e) => {
+    const button = $(e);
+    button.click(() => {
+        const scheduleId = button.data('schedule-id');
+        const userId = button.data('user-id');
+        const candidateId = button.data('candidate-id');
+        const availability = parseInt(button.data('availability'));
+        const nextAvailability = (availability + 1) % 3;
+        $.post(`/schedules/${scheduleId}/users/${userId}/candidates/${candidateId}`,
+            { availability: nextAvailability },
+            (data) => {
+                button.data('availability', data.availability);
+                const availabilityLables = ['❌', '❓', '⭕'];
+                button.text(availabilityLables[data.availability]);
+            });
+    });
+});
