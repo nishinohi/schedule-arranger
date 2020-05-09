8bc1745ad60c450ce72ad8f15c041c9ddef3a21e
nishinohi <tomoya0209@gmail.com>
2020-05-09 16:35:28 +0900

    ログイン後にログイン前に閲覧していたurlにリダイレクトするよう修正

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('login', { user: req.user });
+    const from = req.query.from;
+    if (from) {
+        res.cookie('loginFrom', from, { expires: new Date(Date.now() + 600000) });
+    }
+    res.render('login');
 })
 
 module.exports = router;
